40171841cb15b611dca22fc48f6d735b64c4a7e7
Andrew Herculano <andrewherculano@hotmail.com>
2021-03-19 11:06:51 -0300

    add event submit in form

--- quiz-interativo/app.js
+++ quiz-interativo/app.js
@@ -1,9 +1,25 @@
-const form = document.querySelector('.form-quiz')
+const form = document.querySelector(".form-quiz");
 
-console.log(form)
+const correctAnswers = ["B", "A", "B", "C", "A"];
 
-form.addEventListener('submit', event => {
-    event.preventDefault()
+form.addEventListener("submit", (event) => {
+  event.preventDefault();
 
-    console.log(event.target.inputQuestion1)
-})
+  let scoreUser = 0;
+
+  userAnswers = [
+    event.target.inputQuestion1.value,
+    event.target.inputQuestion2.value,
+    event.target.inputQuestion3.value,
+    event.target.inputQuestion4.value,
+    event.target.inputQuestion5.value,
+  ];
+
+  userAnswers.forEach((userAnswer, index) => {
+    if (userAnswer === correctAnswers[index]) {
+      scoreUser += 20;
+    }
+  });
+
+  console.log(scoreUser);
+});
