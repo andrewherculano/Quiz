c78e8db3d56d06ed87b2b51547d27f4450a16413
Andrew Herculano <andrewherculano@hotmail.com>
2021-03-20 13:36:26 -0300

    insert alerts, porcent and message score

--- quiz-interativo/app.js
+++ quiz-interativo/app.js
@@ -1,50 +1,61 @@
 const form = document.querySelector(".form-quiz");
-const button = document.querySelector(".button");
+const messageScore = document.querySelector(".result-score");
+const messagePorcentScore = document.querySelector(".porcent-score");
 const paragraphScore = document.createElement("p");
 
 const correctAnswers = ["B", "A", "B", "C", "A"];
 let scoreUser = 0;
+let porcentScore = 0;
 
 const checkCorrectsAnswers = (userAnswer, index) => {
   if (userAnswer === correctAnswers[index]) {
     scoreUser += 1;
+    porcentScore += 20;
   }
 };
 
-const insertParagraphAndClassInResult = () => {
-  button.insertAdjacentElement("afterend", paragraphScore);
-  paragraphScore.setAttribute("class", "show-score");
+const insertParagraph = () => {
+  messageScore.insertAdjacentElement("beforeend", paragraphScore);
 };
 
 const showMessageResults = () => {
   switch (scoreUser) {
     case 1:
       paragraphScore.textContent = "Você acertou apenas 1 questão =(";
+      paragraphScore.setAttribute("class", "result-score-message-error");
       break;
     case 2:
       paragraphScore.textContent = "Você acertou apenas 2 questões =(";
+      paragraphScore.setAttribute("class", "result-score-message-error");
       break;
     case 3:
       paragraphScore.textContent = "Você acertou 3 questões =|";
+      paragraphScore.setAttribute("class", "result-score-message-warning");
+
       break;
     case 4:
       paragraphScore.textContent = "Parabéns você acertou 4 questões =)";
+      paragraphScore.setAttribute("class", "result-score-message-correct");
       break;
     case 5:
       paragraphScore.textContent =
         "Parabéns você acertou todas as questões! =)";
+      paragraphScore.setAttribute("class", "result-score-message-correct");
+
       break;
     default:
       paragraphScore.textContent =
         "Poxa você não acertou nenhuma questão, mas não desista. Tente novamente!";
+      paragraphScore.setAttribute("class", "result-score-message-error");
   }
 };
 
 const handleSubmitResultsAndScore = (event) => {
   event.preventDefault();
   scoreUser = 0;
+  porcentScore = 0;
 
-  insertParagraphAndClassInResult();
+  insertParagraph();
 
   userAnswers = [
     event.target.inputQuestion1.value,
@@ -56,6 +67,20 @@ const handleSubmitResultsAndScore = (event) => {
 
   userAnswers.forEach(checkCorrectsAnswers);
   showMessageResults();
+
+  messagePorcentScore.classList.remove("display");
+
+  scrollTo(0, 0);
+
+  let counter = 0;
+
+  const timerPorcentScore = setInterval(() => {
+    if (counter === porcentScore) {
+      clearInterval(timerPorcentScore);
+    }
+    messagePorcentScore.querySelector("span").textContent = `${counter}%`;
+    counter++;
+  }, 10);
 };
 
 form.addEventListener("submit", handleSubmitResultsAndScore);
